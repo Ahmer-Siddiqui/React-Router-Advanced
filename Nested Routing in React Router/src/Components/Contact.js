5da5159920455dc60d774dcff0048f40fd1ad474
Ahmer <siddiquiahmer970@gmail.com>
2023-03-10 00:04:31 -0500

    nesting Router puting

---
+++ Nested Routing in React Router/src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <h1>Contact</h1>
+    </>
+  )
+}
+
+export default Contact
